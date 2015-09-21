46c71a10e5c07009d6940360757af61a8896f5df
faganpe <paul.fagan@bt.com>
2015-09-21 16:40:44 +0100

    Create ES index with Mappings

--- src/scripts/es/test_data.sh
+++ src/scripts/es/test_data.sh
@@ -1,4 +1,4 @@
-curl -XPUT 'http://localhost:9200/twitter/' -d '{
+curl -XPUT 'http://192.168.99.100:9200/twitter/' -d '{
     "settings" : {
         "number_of_shards" : 1,
         "number_of_replicas" : 0
